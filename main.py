be952fd3ba06542ff88715b172a91a16982a9f04
Anna Sakurai <zenasak@gmail.com>
2022-05-31 22:36:33 +0200

    add number buttons with onclick functions to change the clicked buttons.

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
+from multiprocessing.sharedctypes import Value
 import random
 import os
 from pkgutil import iter_modules
 from telnetlib import LOGOUT
 
 
-password = 'shinobi'
+# password = 'shinobi'
 
-my_input_field = Element('my-input-field')
+number_input_field = Element('number-input-field')
 
-def on_click(*args, **kwargs):
-    input_value = my_input_field.element.value
-    message = f"You said: {input_value}"
-    pyscript.write('my-output', message)
+def on_click_number(*args, **kwargs):
+    input_value = number_input_field.element.value
+    if input_value == "1":
+        message = f"{input_value} in Japanese is 壱"
+        pyscript.write('number-output', message)
+    elif input_value == "2":
+        message = f"{input_value} in Japanese is 弐"
+        pyscript.write('number-output', message)
+    elif input_value == "3":
+        message = f"{input_value} in Japanese is 参"
+        pyscript.write('number-output', message)
+    elif input_value == "4":
+        message = f"{input_value} in Japanese is 肆"
+        pyscript.write('number-output', message)
+    elif input_value == "5":
+        message = f"{input_value} in Japanese is 伍"
+        pyscript.write('number-output', message)
+    elif input_value == "6":
+        message = f"{input_value} in Japanese is 陸"
+        pyscript.write('number-output', message)
+    elif input_value == "7":
+        message = f"{input_value} in Japanese is 漆"
+        pyscript.write('number-output', message)
+    elif input_value == "8":
+        message = f"{input_value} in Japanese is 捌"
+        pyscript.write('number-output', message)
+    elif input_value == "9":
+        message = f"{input_value} in Japanese is 玖"
+        pyscript.write('number-output', message)
+        
+        
 
+password_input_field = Element('password-input-field')
+
+
+def on_click_password(*args, **kwargs):
+    input_password = password_input_field.element.value
+    password = 'Correct code. Password is Shinobi'
+    pyscript.write('password-output', password)
--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const containerH1 = document.querySelector(".title");
+const container1 = document.querySelector(".part1");
+const container2 = document.querySelector(".part2");
+const imageContainer = document.querySelector(".image-container");
 
-const createFirstPage = () => {
-  containerH1.innerHTML += `
-        <h1 class="type">Japan</h1>
-        <div class="type">Put in the password: </div>
+const createFirstChallengePart1 = () => {
+  container1.innerHTML += `
+        <h1 class="type">無限城</h1>
+        <p>You have to figure out the pass code in order to get out this Mugen castle. Figure out the numbers that are written in Japanese to crack the code.</p>
+        <div class="type">Type in a number between 1 - 9: </div>
         <div class="input">
         <input
-          id="my-input-field"
+          id="number-input-field"
           class="input-filed border flex item-center mr-3 border-grey-300 p-2"
           type="text"
         />
@@ -14,14 +17,90 @@ const createFirstPage = () => {
           means="enter"
           id="my-button"
           type="submit"
-          pys-onClick="on_click"
+          pys-onClick="on_click_number"
         >
           入力
         </button>
       </div>
-      <div id="my-output"></div>
+      <div id="number-output"></div>
     
         `;
 };
 
-createFirstPage();
+// const createFirstChallengePart2 = () => {
+//   container2.innerHTML += `
+//   <h2>合言葉</h2>
+//   <input id='password-input-field'
+//   class="input-filed border flex item-center mr-3 border-grey-300 p-2"
+//   type="text"
+// />
+// <button
+// means="enter"
+// id="my-button"
+// type="submit"
+// pys-onClick="on_click_password"
+// >
+// 入力
+// </button>
+// <div id="password-output"></div>
+//   `;
+// };
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+let imgNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+function shuffleArray(array) {
+  let curId = array.length;
+  // There remain elements to shuffle
+  while (0 !== curId) {
+    // Pick a remaining element
+    let randId = Math.floor(Math.random() * curId);
+    curId -= 1;
+    // Swap it with the current element.
+    let tmp = array[curId];
+    array[curId] = array[randId];
+    array[randId] = tmp;
+  }
+  return array;
+}
+
+// const pressedButton = (e) => {
+//   e.classList.add("active");
+// };
+
+const pressedButton = () => {
+  document.addEventListener("click", function handleClick(event) {
+    if (event.target.classList.contains("active")) {
+      event.target.classList.remove("active");
+    } else {
+      event.target.classList.add("active");
+    }
+  });
+};
+const kanjiNumbers = () => {
+  let shuffledArray = shuffleArray(imgNumbers);
+
+  for (let i = 0; i < imgNumbers.length; i++) {
+    imageContainer.innerHTML += `
+        <button
+        class="numbers"
+        onclick="pressedButton()"
+        >
+        <img
+        id="${shuffledArray[i] + 1}"
+        style="width: 200px; height: 200px"
+        class="grid-img"
+        src="./assets/0${shuffledArray[i]}.png"
+        alt="${shuffledArray[i] + 1}"
+        />
+        </button>
+        `;
+  }
+};
+
+createFirstChallengePart1();
+// createFirstChallengePart2();
+kanjiNumbers();
