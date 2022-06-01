e0d113a2a8217de7ed46ced51641cbf1798ef7a2
Anna Sakurai <zenasak@gmail.com>
2022-06-01 20:25:58 +0200

    create shinobi.html. click nyurokuBtn to direct to next page if the answer is correct. add input / button & connect to hangman.py.

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const imageContainer = document.querySelector(".image-container");
 
 const createFirstChallengePart1 = () => {
   container1.innerHTML += `
-        <h1 class="type">無限城</h1>
-        <p>You have to figure out the pass code in order to get out this Mugen castle. Figure out the numbers that are written in Japanese to crack the code.</p>
-        <div class="type">Type in a number between 1 - 9: </div>
+        
+        <p>You landed into this mysterious Mugen castle. In order to get out of here to chase after the villain, you need a code.</p>
+        <p>BUT everything seems in Japanese. Figure out the numbers, then press the right buttons.</p>
+        
         <div class="input">
         <input
           id="number-input-field"
           class="input-filed border flex item-center mr-3 border-grey-300 p-2"
           type="text"
+          placeholder="Type in a number"
         />
         <button
           means="enter"
@@ -21,30 +23,35 @@ const createFirstChallengePart1 = () => {
         >
           入力
         </button>
+        <div class='numberIsBox' id="number-output"></div>
       </div>
-      <div id="number-output"></div>
     
         `;
 };
+const kanjiNumbers = () => {
+  let shuffledArray = shuffleArray(imgNumbers);
 
-// const createFirstChallengePart2 = () => {
-//   container2.innerHTML += `
-//   <h2>合言葉</h2>
-//   <input id='password-input-field'
-//   class="input-filed border flex item-center mr-3 border-grey-300 p-2"
-//   type="text"
-// />
-// <button
-// means="enter"
-// id="my-button"
-// type="submit"
-// pys-onClick="on_click_password"
-// >
-// 入力
-// </button>
-// <div id="password-output"></div>
-//   `;
-// };
+  for (let i = 0; i < imgNumbers.length; i++) {
+    imageContainer.innerHTML += `
+        <button
+        class="numbers"
+        onclick="pressedButton()"
+        >
+        <img
+        id="${shuffledArray[i] + 1}"
+        style="width: 200px; height: 200px"
+        class="grid-img"
+        src="./assets/0${shuffledArray[i]}.png"
+        alt="${shuffledArray[i] + 1}"
+        />
+        </button>
+        `;
+  }
+
+  for (let i = 1; i < 10; i++) {
+    document.getElementById(`${i}`).addEventListener("click", pressedButton);
+  }
+};
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -67,37 +74,100 @@ function shuffleArray(array) {
   return array;
 }
 
-// const pressedButton = (e) => {
-//   e.classList.add("active");
-// };
+let num1clicked = false;
+let num2clicked = false;
+let num3clicked = false;
+let num4clicked = false;
+let num5clicked = false;
+let num6clicked = false;
+let num7clicked = false;
+let num8clicked = false;
+let num9clicked = false;
 
-const pressedButton = () => {
-  document.addEventListener("click", function handleClick(event) {
-    if (event.target.classList.contains("active")) {
-      event.target.classList.remove("active");
-    } else {
-      event.target.classList.add("active");
+const pressedButton = (event) => {
+  // document
+  //   .querySelectorAll(".numbers")
+  //   .addEventListener("click", function handleClick(event) {
+  if (event.target.id == "1" && num1clicked == false) {
+    num1clicked = true;
+  }
+  if (event.target.id == "2" && num2clicked == false) {
+    num2clicked = true;
+  }
+  if (event.target.id == "3" && num3clicked == false) {
+    num3clicked = true;
+  }
+  if (event.target.id == "4" && num4clicked == false) {
+    num4clicked = true;
+  }
+  if (event.target.id == "5" && num5clicked == false) {
+    num5clicked = true;
+  }
+  if (event.target.id == "6" && num6clicked == false) {
+    num6clicked = true;
+  }
+  if (event.target.id == "7" && num7clicked == false) {
+    num7clicked = true;
+  }
+  if (event.target.id == "8" && num8clicked == false) {
+    num8clicked = true;
+  }
+  if (event.target.id == "9" && num9clicked == false) {
+    num9clicked = true;
+  }
+  if (event.target.classList.contains("active")) {
+    if (event.target.id == "1" && num1clicked == true) {
+      num1licked = false;
     }
-  });
+    if (event.target.id == "2" && num2clicked == true) {
+      num2clicked = false;
+    }
+    if (event.target.id == "3" && num3clicked == true) {
+      num3clicked = false;
+    }
+    if (event.target.id == "4" && num4clicked == true) {
+      num4clicked = false;
+    }
+    if (event.target.id == "5" && num5clicked == true) {
+      num5clicked = false;
+    }
+    if (event.target.id == "6" && num6clicked == true) {
+      num6clicked = false;
+    }
+    if (event.target.id == "7" && num7clicked == true) {
+      num7clicked = false;
+    }
+    if (event.target.id == "8" && num8clicked == true) {
+      num8clicked = false;
+    }
+    if (event.target.id == "9" && num9clicked == true) {
+      num9clicked = false;
+    }
+    event.target.classList.remove("active");
+  } else {
+    event.target.classList.add("active");
+  }
+  // });
 };
-const kanjiNumbers = () => {
-  let shuffledArray = shuffleArray(imgNumbers);
 
-  for (let i = 0; i < imgNumbers.length; i++) {
-    imageContainer.innerHTML += `
-        <button
-        class="numbers"
-        onclick="pressedButton()"
-        >
-        <img
-        id="${shuffledArray[i] + 1}"
-        style="width: 200px; height: 200px"
-        class="grid-img"
-        src="./assets/0${shuffledArray[i]}.png"
-        alt="${shuffledArray[i] + 1}"
-        />
-        </button>
-        `;
+const num2 = document.getElementById("2");
+const num7 = document.getElementById("7");
+const num9 = document.getElementById("9");
+
+const password = () => {
+  console.log("nyuryoku pressed");
+  if (
+    num2clicked === true &&
+    num7clicked === true &&
+    num9clicked === true &&
+    num1clicked === false &&
+    num3clicked === false &&
+    num4clicked === false &&
+    num5clicked === false &&
+    num6clicked === false &&
+    num8clicked === false
+  ) {
+    window.location.href = "/shinobi.html";
   }
 };
 
