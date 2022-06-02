ad92379ddb6dac07a567db85ace9ec7a8bd94850
Anna Sakurai <zenasak@gmail.com>
2022-06-03 01:49:51 +0200

    connect chouhan.py with a function

--- python/chouhan.py
+++ python/chouhan.py
@@ -1,16 +1,24 @@
 import random
 
-computer = random.randint(0, 1)
 
-choice = [cho, han]
+choice = ['丁', '半']
 
-if player == computer:
-    print(f'{choice[player]}You \n {choice[computer]} Computer \n \n Draw.')
-elif player == 0 and computer == 2:
-    print(f'{choice[player]}You \n {choice[computer]} Computer \n \n You win!')
-elif player == 1 and computer == 0:
-    print(f"{choice[player]}You \n {choice[computer]} Computer \n \n You win!")
-elif player == 2 and computer == 1:
-    print(f'{choice[player]}You \n  {choice[computer]} Computer \n \nYou win!')
-else:
-    print(f'{choice[player]}You \n {choice[computer]}Computer \n \nYou lose!')
+def on_click_chohan(*args, **kwargs):
+    rand = random.randint(0, 1)
+    pyscript.write('modal-content', f"{choice[rand]}")    
+    
+
+
+# computer = random.randint(0, 1)
+
+
+# if player == computer:
+#     print(f'{choice[player]}You \n {choice[computer]} Computer \n \n Draw.')
+# elif player == 0 and computer == 2:
+#     print(f'{choice[player]}You \n {choice[computer]} Computer \n \n You win!')
+# elif player == 1 and computer == 0:
+#     print(f"{choice[player]}You \n {choice[computer]} Computer \n \n You win!")
+# elif player == 2 and computer == 1:
+#     print(f'{choice[player]}You \n  {choice[computer]} Computer \n \nYou win!')
+# else:
+#     print(f'{choice[player]}You \n {choice[computer]}Computer \n \nYou lose!')
--- scripts/sakura.js
+++ scripts/sakura.js
@@ -1,6 +1,6 @@
 const thirdChallenge = document.querySelector(".third-challenge");
-// const choBtn = document.getElementById("cho");
-// const hanBtn = document.getElementById("han");
+const modal = document.getElementById("modal");
+const span = document.querySelector(".close");
 
 const thirdChallengeInit = () => {
   thirdChallenge.innerHTML += `
@@ -13,8 +13,14 @@ const thirdChallengeInit = () => {
         </div>
 
         <div class="cho-han-btns-container">
-            <button class='chohanBtns' id="cho">丁</button>
-            <button class='chohanBtns' id="han">半</button>
+            <button class='chohanBtns' id="cho" pys-onClick="on_click_chohan">丁</button>
+            <button class='chohanBtns' id="han" pys-onClick="on_click_chohan">半</button>
+        </div>
+
+        <div id="modal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <div id='modal-content' style='color:#000'></div>
         </div>
     `;
 
@@ -23,8 +29,19 @@ const thirdChallengeInit = () => {
 };
 
 const btnPressed = (e) => {
-  console.log("btn pressed");
   e.target.classList.toggle("pushed");
+  document.getElementById("modal").style.display = "block";
+
+  document.getElementById("modal-content").innerHTML == "半"
+    ? document.getElementById("soundHan").play()
+    : document.getElementById("soundCho").play();
 };
 
 thirdChallengeInit();
+
+document.querySelector(".close").onclick = () => {
+  document.getElementById("modal").style.display = "none";
+  document.getElementById("cho").classList.contains("pushed")
+    ? document.getElementById("cho").classList.toggle("pushed")
+    : document.getElementById("han").classList.toggle("pushed");
+};
