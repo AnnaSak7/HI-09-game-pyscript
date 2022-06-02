065ee23d5f5392c74003ed364c8027ed762d2690
Anna Sakurai <zenasak@gmail.com>
2022-06-02 12:57:21 +0200

    finish hungman functionality

--- hangman.py
+++ hangman.py
@@ -3,7 +3,7 @@ import random
 
 # from ascii_hangman import stages, logo
 
-words = ['sakura', 'kiku', 'ran', 'botan']
+words = ['sakura', 'kiku', 'shinobi', 'botan']
 
 
 
@@ -14,7 +14,6 @@ display = []
 for x in range(len(chosen_word)):
     display += '_'
 
-pyscript.write('msg', f'Guess a letter {chosen_word}')
 
 pyscript.write('unknown-letters', f"{' '.join(display)}")
 
@@ -22,45 +21,14 @@ letter_input = Element('letter_input')
 
 def on_click_letter(*args, **kwargs):
     input_value = letter_input.element.value
-    pyscript.write('response', f"guessed letter is {input_value}")
     for index in range(len(chosen_word)):
-        if chosen_word[index] == input_value:
-            display[index] = input_value
+        if chosen_word[index] == input_value.lower():
+            display[index] = input_value.lower()
     pyscript.write('unknown-letters', f"{' '.join(display)}")
-
+    letter_input.element.value = ""
+    
+    
             
-
-# end_of_game = False
-# guessed_letters = []
-
-# while not end_of_game:
-#     guess = letter_input.element.value
-    
-#     if guess in chosen_word:
-#         for index in range(len(chosen_word)):
-#             if chosen_word[index] == guess:
-#                 display[index] = guess
-        # pyscript.write('unknown-letters', f"{' '.join(display)}")
-
-#     if guess in display:
-#            print("You have already guessed this letter.")
-#     elif guess in chosen_word:
-#         for index in range(len(chosen_word)):
-#             if chosen_word[index] == guess:
-#                 display[index] = guess
-#         print(f"{' '.join(display)}")
-#     else:
-#         lives-=1
-#         print(f"You guessed {guess}, that's not in the word. You lose a life.")
-#         print(stages[lives])
-#         print(f"{lives} lives left")
-    
-#     guessed_letters.append(guess)
-#     if lives == 0:
-#         print('You lose!')
-#         quit()
-    
-#     if "_" not in display:
-#         end_of_game = True
-#         print("You win!")
+# if ' '.join(display) == chosen_word:
+#     pyscript.write('key', f"You did it! Change shinobi.html to sakura.html in the url.")
 
--- shinobi.js
+++ shinobi.js
@@ -1,17 +1,20 @@
 const secondChallenge = document.querySelector(".second-challenge");
-
+const guessedWord = document.getElementById("unknown-letters");
 const secondChallengeInit = () => {
   secondChallenge.innerHTML += `
     <py-title>Hangman</py-title>
-      <div id="shinobi">
-        <div id="msg" style="color: #000"></div>
+      <div id="input-btn-container">
         <input id="letter_input"></input>
         <button id="guess_letter_btn" pys-onClick="on_click_letter">Click</button>
-        <div id="response"></div>
       </div>
       <div id="unknown-letters"></div>
+      <div id='key'></div>
       <py-script src="hangman.py"></py-script>
     `;
 };
+
+// if (!guessedWord.includes("_")) {
+//   alert("hey you did it!");
+// }
 
 secondChallengeInit();
